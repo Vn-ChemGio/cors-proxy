8d02885164c87547c497b5ec5e04c7f80bccc5f4
WindBlade <vn.chemgio@yahoo.com>
2024-07-06 21:47:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const cors = require('cors')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
-app.use(cors())
-app.use(createProxyMiddleware({
-    router: (req) => new URL(req.path.substring(1)),
-    pathRewrite: (path, req) => (new URL(req.path.substring(1))).pathname,
-    changeOrigin: true,
-    logger: console
+app.use(cors({
+    origin: [
+        'http://localhost:*',
+        'https://h5.zdn.vn',
+        'zbrowser://h5.zdn.vn'
+    ],
 }))
+
+app.use(
+    createProxyMiddleware({
+      target: 'http://kythuatvov.vn:8080/vovmedia_bak',
+      changeOrigin: true,
+    }),
+);
 
 app.listen(3000, () => {
     console.info('proxy server is running on port 8088')
